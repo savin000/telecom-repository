1fb9cc892e3bc956d83558c6d74f02d1967cafc4
savin000 <58307835+savin000@users.noreply.github.com>
2021-01-09 00:18:32 +0300

    Injection annotation added

---
+++ src/main/java/com/savin/annotations/AutoInjectable.java
@@ -0,0 +1,18 @@
+package com.savin.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation is used to mark fields for dependency injection
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface AutoInjectable {
+    /**
+     * @return injectable class
+     */
+    Class<?> clazz() default Object.class;
+}
