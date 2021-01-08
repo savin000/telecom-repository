1ce4e6049e9a255b1d48f0a88821ca903a3119e0
savin000 <58307835+savin000@users.noreply.github.com>
2021-01-09 00:21:50 +0300

    Dependency injection configuration annotation added

---
+++ src/main/java/com/savin/annotations/Configuration.java
@@ -0,0 +1,19 @@
+package com.savin.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Configuration annotation
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Configuration {
+
+    /**
+     * @return packages where necessary classes are stored
+     */
+    String[] packages();
+}
