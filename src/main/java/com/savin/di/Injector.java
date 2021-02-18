08bff764e88b579c87edf64e42d87d1f472fcb51
savin000 <58307835+savin000@users.noreply.github.com>
2021-02-18 15:14:42 +0300

    Refactored

--- src/main/java/com/savin/di/Injector.java
+++ src/main/java/com/savin/di/Injector.java
@@ -3,6 +3,8 @@ package com.savin.di;
 import com.google.common.reflect.ClassPath;
 import com.savin.annotations.AutoInjectable;
 import com.savin.annotations.Configuration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -19,44 +21,50 @@ import java.util.Set;
  */
 @Configuration(packages = {"com.savin"})
 public class Injector {
+    private static final Logger LOG = LogManager.getLogger();
 
     /**
      * This method implements dependency injection
      *
      * @param obj the object to inject dependencies
      * @param <E> generic type of the class
-     * @throws IllegalAccessException illegal access
-     * @throws IOException failed I/O operation
-     * @throws InstantiationException something went wrong during instantiation
-     * @throws NoInjectableClassesException if there are no injectable classes
-     * @throws SurplusOfInjectableClassesException if there are too many injectable classes
+     * @throws InjectionException if something went wrong during injection
      */
-    public static <E> void inject(E obj) throws
-            NoInjectableClassesException, SurplusOfInjectableClassesException {
-        ClassPath classPath = ClassPath.from(Thread.currentThread().getContextClassLoader());
+    public static <E> void inject(E obj) throws InjectionException {
+        ClassPath classPath = null;
+        try {
+            classPath = ClassPath.from(Thread.currentThread().getContextClassLoader());
+        } catch (IOException e) {
+            LOG.info(e);
+        }
 
         for (String pkg : Injector.class.getAnnotation(Configuration.class).packages()) {
+            assert classPath != null;
             Set<ClassPath.ClassInfo> allClasses = classPath.getTopLevelClassesRecursive(pkg);
             for (Field field : obj.getClass().getDeclaredFields()) {
                 if (field.isAnnotationPresent(AutoInjectable.class)) {
                     field.setAccessible(true);
                     Class<?> clazz = field.getAnnotation(AutoInjectable.class).clazz();
                     List<Object> instances = new ArrayList<>();
-                    for (ClassPath.ClassInfo ci : allClasses) {
-                        if (clazz.isAssignableFrom(ci.load()) && !ci.load().isInterface()) {
-                            instances.add(ci.load().newInstance());
+                    try {
+                        for (ClassPath.ClassInfo ci : allClasses) {
+                            if (clazz.isAssignableFrom(ci.load()) && !ci.load().isInterface()) {
+                                instances.add(ci.load().newInstance());
+                            }
                         }
-                    }
-                    if (Collection.class.isAssignableFrom(field.getType())) {
-                        field.set(obj, instances);
-                    } else {
-                        if (instances.size() == 1) {
-                            field.set(obj, instances.get(0));
-                        } else if (instances.size() > 1) {
-                            throw new SurplusOfInjectableClassesException("Surplus of injectable classes");
+                        if (Collection.class.isAssignableFrom(field.getType())) {
+                            field.set(obj, instances);
                         } else {
-                            throw new NoInjectableClassesException("No injectable classes");
+                            if (instances.size() == 1) {
+                                field.set(obj, instances.get(0));
+                            } else if (instances.size() > 1) {
+                                throw new InjectionException("Surplus of injectable classes");
+                            } else {
+                                throw new InjectionException("No injectable classes");
+                            }
                         }
+                    } catch (IllegalAccessException | InstantiationException e) {
+                        throw new InjectionException("Reflection Error ", e);
                     }
                 }
             }
