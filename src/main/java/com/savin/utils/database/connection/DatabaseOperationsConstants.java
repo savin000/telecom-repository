aa7a783503538b9a4d610472f851f54415816afe
savin000 <58307835+savin000@users.noreply.github.com>
2021-04-06 21:32:12 +0300

    Add some classes for jdbc

---
+++ src/main/java/com/savin/utils/database/connection/DatabaseOperationsConstants.java
@@ -0,0 +1,12 @@
+package com.savin.utils.database.connection;
+
+public final class DatabaseOperationsConstants {
+
+    public static final String DRIVER_PROPERTY_NAME = "database.driver";
+
+    public static final String URL_PROPERTY_NAME = "database.url";
+
+    public static final String USER_PROPERTY_NAME = "database.user";
+
+    public static final String PASSWORD_PROPERTY_NAME = "database.password";
+}
