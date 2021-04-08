31e06dc7a933549728a96d8417c5e1e47edaf35c
savin000 <58307835+savin000@users.noreply.github.com>
2021-04-08 21:48:17 +0300

    Add Javadoc comments

--- src/main/java/com/savin/utils/database/DatabaseOperationsConstants.java
+++ src/main/java/com/savin/utils/database/DatabaseOperationsConstants.java
@@ -1,12 +1,30 @@
 package com.savin.utils.database;
 
+/**
+ * This class stores constants for DatabaseOperations class
+ *
+ * @author Mikhail Savin
+ * @since 1.0
+ */
 public final class DatabaseOperationsConstants {
 
+    /**
+     * Database driver property name
+     */
     public static final String DRIVER_PROPERTY_NAME = "database.driver";
 
+    /**
+     * Database URL property name
+     */
     public static final String URL_PROPERTY_NAME = "database.url";
 
+    /**
+     * Database user property name
+     */
     public static final String USER_PROPERTY_NAME = "database.user";
 
+    /**
+     * User's password property name
+     */
     public static final String PASSWORD_PROPERTY_NAME = "database.password";
 }
